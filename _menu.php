ef51e27c5058fe1ec3b5bc8f095c6034ff8b0db0
Spyros Avlonitis <spyrosavl@gmail.com>
2017-12-26 23:59:03 +0200

    menu

---
+++ _menu.php
@@ -0,0 +1,147 @@
+<div class="masthead">
+  <a href="/"><h3 class="text-muted">IKA</h3></a>
+  
+  <div class="navbar navbar-default yamm">
+    <div class="navbar-header">
+      <button type="button" data-toggle="collapse" data-target="#navbar-collapse-2" class="navbar-toggle"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><!-- <a href="/" class="navbar-brand">Αρχική</a> -->
+    </div>
+    <div id="navbar-collapse-2" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        <!-- Media Example -->
+        <li class="dropdown"><a href="#" data-toggle="dropdown" class="dropdown-toggle">Ασφαλισμένοι<b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li>
+              <div class="yamm-content">
+                
+                <div class="row">
+                  <div class="col-md-6">
+                    <ul class="media-list">
+                      <li class="media">
+                        <div class="media-body">
+                          <h4 class="media-heading"><a href="/">Ηλεκτρονικές Υπηρεσίες</a></h4>Όπως: Εκδοση ΑΜΚΑ, ..., ....
+                        </div>
+                      </li>
+                      <li class="media">
+                        <div class="media-body">
+                          <h4 class="media-heading"><a href="/">Συχνές Ερωτήσεις/Δικαιολογητικά</a></h4>Όπως: Εκδοση ΑΜΚΑ, ..., ....
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="col-md-6">
+                    <h4>Δημοφιλέις Εργασίες</h4>
+                    <ul style="list-style-type: none;">
+                      <li>
+                        <h5 class="media-heading"><a href="/">Νέος Ασφαλισμένος</a></h5>
+                      </li>
+                      <li>
+                        <h5 class="media-heading"><a href="/">Ασφαλιστική Ικανότητα</a></h5>
+                      </li>
+                      <li>
+                        <h5 class="media-heading"><a href="/">ΑΜΚΑ</a></h5>
+                      </li>
+                      <li>
+                        <h5 class="media-heading"><a href="/">Αναζήτηση Γιατρού ΕΟΠΥΥ</a></h5>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="col-md-12">
+                    <br/>
+                    <!-- searchbar -->
+                    <form class="" role="search">
+                      <h5>Αναζήτηση</h5>
+                      <div class="input-group" style="width: 100%;">
+                        <input type="text" class="form-control" placeholder="Αναζήτηση στην κατηγορία 'Ασφαλισμένοι'" name="q">
+                        <div class="input-group-btn">
+                          <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+                
+              </div>
+            </li>
+          </ul>
+        </li>
+
+        <li class="dropdown"><a href="#" data-toggle="dropdown" class="dropdown-toggle">Εργοδότες<b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li>
+              <div class="yamm-content">
+                <ul class="media-list">
+                  <li class="media"><a href="#" class="pull-right"><img src="demo/img/64x64.gif" alt="64x64" class="media-object"></a>
+                    <div class="media-body">
+                      <h4 class="media-heading">Media heading</h4>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante.
+                    </div>
+                  </li>
+                  <li class="media"><a href="#" class="pull-right"><img src="demo/img/64x64.gif" alt="64x64" class="media-object"></a>
+                    <div class="media-body">
+                      <h4 class="media-heading">Media heading</h4>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque.
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </li>
+
+        <li class="dropdown"><a href="#" data-toggle="dropdown" class="dropdown-toggle">Συντάξεις<b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li>
+              <div class="yamm-content">
+                <ul class="media-list">
+                  <li class="media"><a href="#" class="pull-right"><img src="demo/img/64x64.gif" alt="64x64" class="media-object"></a>
+                    <div class="media-body">
+                      <h4 class="media-heading">Media heading</h4>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante.
+                    </div>
+                  </li>
+                  <li class="media"><a href="#" class="pull-right"><img src="demo/img/64x64.gif" alt="64x64" class="media-object"></a>
+                    <div class="media-body">
+                      <h4 class="media-heading">Media heading</h4>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque.
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </li>
+
+        <li class="dropdown"><a href="#" data-toggle="dropdown" class="dropdown-toggle">Συνεργάτες<b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li>
+              <div class="yamm-content">
+                <ul class="media-list">
+                  <li class="media"><a href="#" class="pull-right"><img src="demo/img/64x64.gif" alt="64x64" class="media-object"></a>
+                    <div class="media-body">
+                      <h4 class="media-heading">Media heading</h4>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante.
+                    </div>
+                  </li>
+                  <li class="media"><a href="#" class="pull-right"><img src="demo/img/64x64.gif" alt="64x64" class="media-object"></a>
+                    <div class="media-body">
+                      <h4 class="media-heading">Media heading</h4>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque.
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </li>
+
+        <li><a href="/contact_points.html" class="dropdown-toggle">Σημειά Εξυπηρέτησης</a></li>
+
+        <li>
+          <form class="navbar-form" role="search">
+            <div class="input-group">
+              <input type="text" class="form-control" placeholder="Αναζήτηση" name="q">
+              <div class="input-group-btn">
+                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+              </div>
+            </div>
+          </form>
+        </li>
+      </ul>
+    </div>
+  </div>
+  
+</div>
---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>IKA</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link href="./assets/css/justified-nav.css" rel="stylesheet">
+    <link href="./assets/css/theme.css" rel="stylesheet">
+    <link href="./assets/css/yamm.css" rel="stylesheet">
+    <link href="./assets/css/app.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!-- https://www.digitalocean.com/community/tutorials/how-to-install-linux-apache-mysql-php-lamp-stack-on-ubuntu-16-04 -->
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <!-- The justified navigation menu is meant for single line per list item.
+           Multiple lines will require custom code not provided by Bootstrap. -->
+      <?php include('./_menu.php'); ?>
+
+      <!-- Jumbotron -->
+      <div class="jumbotron">
+        <h1>Marketing stuff!</h1>
+        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
+        <p><a class="btn btn-lg btn-info" href="#" role="button">Get started today</a></p>
+      </div>
+
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-lg-4">
+          <h2>Safari bug warning!</h2>
+          <p class="text-danger">As of v9.1.2, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.</p>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+       </div>
+        <div class="col-lg-4">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
+          <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+        </div>
+      </div>
+
+      <?php include('./_footer.php'); ?>
+  </body>
+</html>
