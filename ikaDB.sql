8d916148c5301003b69b6f6c8bccfae969012e22
Spyros Avlonitis <spyrosavl@gmail.com>
2018-01-09 11:30:13 +0200

    db

--- ikaDB.sql
+++ ikaDB.sql
@@ -30,8 +30,9 @@ CREATE TABLE `users` (
   `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `password` varchar(256) NOT NULL,
   `loginAttempts` int(11) DEFAULT NULL,
+  `user_type` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +41,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'John','Doe','john@example.com','2018-01-05 16:45:22','51616516',NULL),(2,'John','Doe','john@example.com','2018-01-05 16:45:26','51616516',NULL),(3,'John','Doe','john@example.com','2018-01-05 16:45:28','51616516',NULL),(4,'John','Doe','john@example.com','2018-01-05 16:45:28','51616516',NULL),(5,'John','Doe','john@example.com','2018-01-05 16:45:29','51616516',NULL),(6,'John','Doe','john@example.com','2018-01-05 16:45:29','51616516',NULL),(7,'John','Doe','john@example.com','2018-01-05 16:45:30','51616516',NULL),(8,'John','Doe','john@example.com','2018-01-05 16:49:33','51616516',NULL),(9,':fName',':lName',':email','2018-01-05 17:19:05',':password',NULL),(10,':fName',':lName',':email','2018-01-05 17:32:49',':password',NULL),(11,':fName',':lName',':email','2018-01-05 17:34:16',':password',NULL),(12,'spyros','spyros','fsasd','2018-01-05 17:36:19','ac9cd720d97345f342f9125e5bf7c74761e3ff1abaf5221027e3ab90527195a5',NULL),(13,'spyros','spyros','fsasd','2018-01-05 18:17:24','ac9cd720d97345f342f9125e5bf7c74761e3ff1abaf5221027e3ab90527195a5',NULL),(14,'spyros','spyros','fsasd','2018-01-05 18:17:25','ac9cd720d97345f342f9125e5bf7c74761e3ff1abaf5221027e3ab90527195a5',NULL),(15,'spyros','spyros','fsasd','2018-01-05 18:17:30','ac9cd720d97345f342f9125e5bf7c74761e3ff1abaf5221027e3ab90527195a5',NULL),(16,'spyros','spyros','fsasd','2018-01-05 18:17:53','ac9cd720d97345f342f9125e5bf7c74761e3ff1abaf5221027e3ab90527195a5',NULL);
+INSERT INTO `users` VALUES (17,'Spyros','Spyros','spyrosavl@gmail.com','2018-01-08 17:26:11','8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92',NULL,NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +54,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-01-05 20:36:19
+-- Dump completed on 2018-01-09 11:29:15
