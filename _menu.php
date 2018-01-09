8d3d63a362de9e29a04589d6943b847cd46805d5
kelly <kelly.myrsilidi@gmail.com>
2018-01-09 15:05:20 +0200

    toftiaksame

--- _menu.php
+++ _menu.php
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-=======
+
 <?php 
   include ('db_connection.php');
   if(isset($_SESSION["user_id"])){
@@ -11,15 +10,12 @@
   }
 ?>
 
->>>>>>> 148a21c34a32b4046fe81386790a493595735229
 <div class="right-tools pull-right">
   <span id="access">Μέγεθος Γραμμάτων <input  type="range" min="1" max="2" value="1" step="0.5" onchange="$('#body').attr('style', 'font-size: ' + (this.value*100).toString() + '% !important');" ></span>
   <span id="access">Φωνητική Λειτουργία <span style="cursor: pointer;" class="glyphicon glyphicon-headphones"></span></span>
   <br>
   <span class="pull-right" style="border: solid 1px;padding: 5px;border-radius: 2px;"> 
-<<<<<<< HEAD
-    <a href="">Σύνδεση</a> | <a href="">Εγγραφή</a>  <span id="phone_and_email">| <span class="glyphicon glyphicon-earphone"></span> 6982140569 <span class="glyphicon glyphicon-envelope"></span> info@ika.gr </span>| <a href="">English</a>
-=======
+
     
     <?php if (isset($user)): ?>
       <span class="glyphicon glyphicon-user"></span> <a href="/update_profile.php"><?= $user['firstName'] ?> <?= $user['lastName'] ?></a> / <a href="/authenticate.php?a=logout">Αποσύνδεση</a> 
@@ -28,7 +24,6 @@
     <?php endif; ?>
 
     <span id="phone_and_email">| <strong><span class="glyphicon glyphicon-earphone"></span>21045454545 <span class="glyphicon glyphicon-envelope"></span> info@ika.gr </span></strong>| <a href="">English</a>
->>>>>>> 148a21c34a32b4046fe81386790a493595735229
   </span>
 </div>
 
@@ -52,11 +47,8 @@
                     <ul class="media-list">
                       <li class="media">
                         <div class="media-body">
-<<<<<<< HEAD
-                          <h4 class="media-heading"><a href="/">Ηλεκτρονικές Υπηρεσίες</a></h4>Όπως: Εκδοση ΑΜΚΑ, ..., ....
-=======
+
                           <h4 class="media-heading"><a href="/">Ηλεκτρονικές Υπηρεσίες</a></h4>Όπως: Υπολογισμός Ενσήμων, ..., ....
->>>>>>> 148a21c34a32b4046fe81386790a493595735229
                         </div>
                       </li>
                       <li class="media">
@@ -86,16 +78,10 @@
                   <div class="col-md-12">
                     <br/>
                     <!-- searchbar -->
-<<<<<<< HEAD
-                    <form class="" role="search">
-                      <h5>Αναζήτηση</h5>
-                      <div class="input-group" style="width: 100%;">
-=======
                     <form class="" role="search" action="search.php">
                       <h5>Αναζήτηση</h5>
                       <div class="input-group" style="width: 100%;">
                         <input type="hidden" name="c" value="Ασφαλισμένοι">
->>>>>>> 148a21c34a32b4046fe81386790a493595735229
                         <input type="text" class="form-control" placeholder="Αναζήτηση στην κατηγορία 'Ασφαλισμένοι'" name="q">
                         <div class="input-group-btn">
                           <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
@@ -114,20 +100,6 @@
           <ul class="dropdown-menu">
             <li>
               <div class="yamm-content">
-<<<<<<< HEAD
-                <ul class="media-list">
-                  <li class="media"><a href="#" class="pull-right"><img src="demo/img/64x64.gif" alt="64x64" class="media-object"></a>
-                    <div class="media-body">
-                      <h4 class="media-heading">Media heading</h4>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante.
-                    </div>
-                  </li>
-                  <li class="media"><a href="#" class="pull-right"><img src="demo/img/64x64.gif" alt="64x64" class="media-object"></a>
-                    <div class="media-body">
-                      <h4 class="media-heading">Media heading</h4>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque.
-                    </div>
-                  </li>
-                </ul>
-=======
                 
                 <div class="row">
                   <div class="col-md-6">
@@ -171,7 +143,6 @@
                   </div>
                 </div>
                 
->>>>>>> 148a21c34a32b4046fe81386790a493595735229
               </div>
             </li>
           </ul>
@@ -181,20 +152,6 @@
           <ul class="dropdown-menu">
             <li>
               <div class="yamm-content">
-<<<<<<< HEAD
-                <ul class="media-list">
-                  <li class="media"><a href="#" class="pull-right"><img src="demo/img/64x64.gif" alt="64x64" class="media-object"></a>
-                    <div class="media-body">
-                      <h4 class="media-heading">Media heading</h4>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante.
-                    </div>
-                  </li>
-                  <li class="media"><a href="#" class="pull-right"><img src="demo/img/64x64.gif" alt="64x64" class="media-object"></a>
-                    <div class="media-body">
-                      <h4 class="media-heading">Media heading</h4>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque.
-                    </div>
-                  </li>
-                </ul>
-=======
                 
                 <div class="row">
                   <div class="col-md-6">
@@ -238,7 +195,6 @@
                   </div>
                 </div>
                 
->>>>>>> 148a21c34a32b4046fe81386790a493595735229
               </div>
             </li>
           </ul>
@@ -247,22 +203,7 @@
         <li class="dropdown"><a href="#" data-toggle="dropdown" class="dropdown-toggle">Συνεργάτες<b class="caret"></b></a>
           <ul class="dropdown-menu">
             <li>
-              <div class="yamm-content">
-<<<<<<< HEAD
-                <ul class="media-list">
-                  <li class="media"><a href="#" class="pull-right"><img src="demo/img/64x64.gif" alt="64x64" class="media-object"></a>
-                    <div class="media-body">
-                      <h4 class="media-heading">Media heading</h4>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante.
-                    </div>
-                  </li>
-                  <li class="media"><a href="#" class="pull-right"><img src="demo/img/64x64.gif" alt="64x64" class="media-object"></a>
-                    <div class="media-body">
-                      <h4 class="media-heading">Media heading</h4>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque.
-                    </div>
-                  </li>
-                </ul>
-=======
-                
+              <div class="yamm-content">                
                 <div class="row">
                   <div class="col-md-6">
                     <ul class="media-list">
@@ -308,17 +249,12 @@
                   </div>
                 </div>
                 
->>>>>>> 148a21c34a32b4046fe81386790a493595735229
               </div>
             </li>
           </ul>
         </li>
 
-<<<<<<< HEAD
-        <li><a href="/contact_points.html" class="dropdown-toggle">Σημεία Εξυπηρέτησης</a></li>
-=======
         <li><a href="/ServicePoints.php" class="dropdown-toggle">Σημεία Εξυπηρέτησης</a></li>
->>>>>>> 148a21c34a32b4046fe81386790a493595735229
 
         <li style="width: 210px;" id="menuSearch">
           <form class="navbar-form" role="search">
