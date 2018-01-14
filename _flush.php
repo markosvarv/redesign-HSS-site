859d9815194278426277265fff96124642defff2
Spyros Avlonitis <spyrosavl@gmail.com>
2018-01-14 20:45:29 +0200

    edit fix

--- _flush.php
+++ _flush.php
@@ -4,12 +4,12 @@
 	}
 	if ($_SESSION['flush'] && $_SESSION['flush']['error']){
 		echo '<div class="alert alert-danger">
-  			<strong>Error!</strong> '.$_SESSION['flush']['error'].'
+  			'.$_SESSION['flush']['error'].'
 		</div>';
 	}
 	if ($_SESSION['flush'] && $_SESSION['flush']['success']){
 		echo '<div class="alert alert-success">
-  			<strong>Success!</strong> '.$_SESSION['flush']['success'].'
+  			'.$_SESSION['flush']['success'].'
 		</div>';
 	}
 	$_SESSION['flush'] = null;
